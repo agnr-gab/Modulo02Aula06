71c3ef56befd12d121e04ca29b0db9ac272d56dd
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-30 13:35:09 -0300

    Lista de exercicios 04

---
+++ src/br/com/zup/Lista04Ex04.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+public class Lista04Ex04 {
+
+    public static void main(String[] args) {
+	// 4 - Escreva, usando while, um programa para calcular a média de N números. O valor de N é dado pelo usuário.
+
+
+    }
+}
