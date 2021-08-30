3c560870ad3ccc75864ff6a9882f58bd3e67671c
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-30 14:36:50 -0300

    resposta exercicio 03

--- src/br/com/zup/Lista04Ex03.java
+++ src/br/com/zup/Lista04Ex03.java
@@ -5,24 +5,21 @@ import java.util.Scanner;
 public class Lista04Ex03 {
     public static void main(String[] args) {
     // 3 - Faça um programa para ler 5 números e mostrar o resultado da soma desses números.
-        Scanner leitorSoma = new Scanner(System.in);
-        System.out.println("Digite 5 números:");
 
-        double numeroInserido = leitorSoma.nextDouble();
+        Scanner leitorSoma = new Scanner(System.in);
+        System.out.println("Digite 5 números.");
+
+
         double soma = 0;
         int contador = 0;
 
-        while (contador < 4) {
-            System.out.println("Digite um número");
-            double valorInserido = leitorSoma.nextDouble();
+        while (contador < 5) {
+            System.out.println("Digite o número:");
+            double numeroInserido = leitorSoma.nextDouble();
             soma = soma + numeroInserido;
             contador++;
         }
         System.out.println("A soma de todos os números inseridos é de: " +soma);
-
-        /*for(int contador = 0; contador < 5; contador++) {
-            System.out.println(contador);         }
-*/
 
     }
 }
