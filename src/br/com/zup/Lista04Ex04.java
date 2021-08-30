098593b11f57237bf8df90e2efe9d49fc2d7221b
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-30 16:13:36 -0300

    resolução exercicio 04

--- src/br/com/zup/Lista04Ex04.java
+++ src/br/com/zup/Lista04Ex04.java
@@ -5,22 +5,7 @@ import java.util.Scanner;
 public class Lista04Ex04 {
 
     public static void main(String[] args) {
-        Scanner leitorMedia = new Scanner(System.in);
-        // 3 - Faça um programa para ler 5 números e mostrar o resultado da soma desses números.
-        System.out.println("Digite a quantidade de 5 números, por favor:");
+        //4 - Escreva, usando while, um programa para calcular a média de N números. O valor de N é dado pelo usuário.
 
-        double qtdNumeros = leitorMedia.nextDouble();
-        double soma = 0;
-        double media = 0;
-        int contador = 1;
-
-        while (contador < qtdNumeros) {
-            System.out.println("Digite o número " +contador++);
-            double valorInserido = leitorMedia.nextDouble();
-            soma = soma + valorInserido;
-            contador++;
-        }
-        media = soma / qtdNumeros;
-        System.out.println("A média de todos os números inseridos é de: " +media);
     }
 }
