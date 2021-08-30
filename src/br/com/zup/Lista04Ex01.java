1229f1abcb90df34fbf8d55563ecd45e22421125
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-30 16:40:57 -0300

    exercicio 01 lista 04

--- src/br/com/zup/Lista04Ex01.java
+++ src/br/com/zup/Lista04Ex01.java
@@ -1,15 +1,15 @@
 package br.com.zup;
 
-import java.util.Scanner;
-
 public class Lista04Ex01 {
+
     public static void main(String[] args) {
+
         //1 - Faça um programa que mostra na tela os números de 1 a 100.
 
-        int i = 1;
-        for (i = 1; i >= 100; ++i) {
-            System.out.println("Contagem em andamento: " + i);
-            if (i == 100) {
+        for (int contador = 1; contador <= 100; contador++) {
+            System.out.println("Contagem em andamento: " + contador);
+
+            if (contador == 100) {
                 System.out.println("Contador finalizado!");
             }
         }
