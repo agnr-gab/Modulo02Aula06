8791805bbb35c91f910e517fa992e57f7da3fa33
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-30 14:12:42 -0300

    resolução exercicios

---
+++ src/br/com/zup/Lista04Ex03.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Lista04Ex03 {
+    public static void main(String[] args) {
+
+    }
+}
+
--- src/br/com/zup/Lista04Ex04.java
+++ src/br/com/zup/Lista04Ex04.java
@@ -1,10 +1,26 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Lista04Ex04 {
 
     public static void main(String[] args) {
-	// 4 - Escreva, usando while, um programa para calcular a média de N números. O valor de N é dado pelo usuário.
+        Scanner leitorMedia = new Scanner(System.in);
+        // 3 - Faça um programa para ler 5 números e mostrar o resultado da soma desses números.
+        System.out.println("Digite a quantide de números, por favor:");
 
+        double qtdNumeros = leitorMedia.nextDouble();
+        double soma = 0;
+        double media = 0;
+        int contador = 0;
 
+        while (contador < qtdNumeros) {
+            System.out.println("Digite um número");
+            double valorInserido = leitorMedia.nextDouble();
+            soma = soma + valorInserido;
+            contador++;
+        }
+        media = soma / qtdNumeros;
+        System.out.println("A média de todos os números inseridos é de: " +media);
     }
 }
